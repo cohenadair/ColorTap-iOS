c6cdbab999ee4656dc251363c39b0a06bc2f9e34
Cohen Adair <cohenadair@gmail.com>
2015-07-18 10:52:24 -0400

    Finished up renaming

---
+++ TapThatColour/TapThatColour/Nodes/CAButtonNode.h
@@ -0,0 +1,15 @@
+//
+//  CAButtonNode.h
+//  TapThatColor
+//
+//  Created by Cohen Adair on 2015-07-18.
+//  Copyright (c) 2015 Cohen Adair. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface CAButtonNode : SKSpriteNode
+
+
+
+@end
---
+++ TapThatColour/TapThatColour/Nodes/CAButtonNode.m
@@ -0,0 +1,13 @@
+//
+//  CAButtonNode.m
+//  TapThatColor
+//
+//  Created by Cohen Adair on 2015-07-18.
+//  Copyright (c) 2015 Cohen Adair. All rights reserved.
+//
+
+#import "CAButtonNode.h"
+
+@implementation CAButtonNode
+
+@end
