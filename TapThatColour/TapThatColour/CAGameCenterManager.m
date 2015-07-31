78fe4d9b3dceb9b510e2dd84b43a581fe6adefc7
Cohen Adair <cohenadair@gmail.com>
2015-07-30 23:54:01 -0400

    Fixed crashing bug for users that don't have Game Centre enabled

--- TapThatColour/TapThatColour/CAGameCenterManager.m
+++ TapThatColour/TapThatColour/CAGameCenterManager.m
@@ -69,6 +69,9 @@
 }
 
 - (void)reportScore:(NSInteger)aScore {
+    if (!self.isEnabled)
+        return;
+    
     GKScore *score = [[GKScore alloc] initWithLeaderboardIdentifier:self.leaderboardId];
     score.value = aScore;
     
